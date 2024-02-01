c07a71f9219461827e333c4bb5b2110adedab23a
안춘모 <zxccns@gmail.com>
2024-02-01 16:12:33 +0900

    feat: Add StartViewController

---
+++ stinger/Component/ViewControllers/StartViewController.swift
@@ -0,0 +1,33 @@
+//
+//  StartViewController.swift
+//  stinger
+//
+//  Created by 안춘모 on 2/1/24.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        
+        print("hello")
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
