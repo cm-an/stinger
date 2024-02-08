2b7f333283ae7e5e7574e3607db37ef2f73f3ea0
모리스 <zxccns@gmail.com>
2024-02-08 10:47:39 +0900

    feat: Add UIColor extension

---
+++ stinger/Common/Extension/UIColor+Extension.swift
@@ -0,0 +1,36 @@
+//
+//  UIColor+Extension.swift
+//  stinger
+//
+//  Created by 안춘모 on 2/7/24.
+//
+
+import UIKit
+
+public extension UIColor {
+    // MARK: - red
+    static func red_ff5600() -> UIColor {
+        return UIColor(red: 1.0, green: 0.339, blue: 0.0, alpha: 1.0)
+    }
+    
+    // MARK: - blue
+    static func blue_69c7f0() -> UIColor {
+        return UIColor(red: 0.412, green: 0.78, blue: 0.941, alpha: 1.0)
+    }
+    static func blue_153982() -> UIColor {
+        return UIColor(red: 0.083, green: 0.225, blue: 0.508, alpha: 1.0)
+    }
+    
+    // MARK: - yellow
+    static func yellow_ffe800() -> UIColor {
+        return UIColor(red: 1.0, green: 0.91, blue: 0.0, alpha: 1.0)
+    }
+    
+    // MARK: - purple
+    static func purple_fb61ff() -> UIColor {
+        return UIColor(red: 0.482, green: 0.38, blue: 1.0, alpha: 1.0)
+    }
+    
+    // MARK: - gray
+    
+}
